30022093e898bc44a350d8b45c8b1009d19b3c41
kehindeag <kenagboola@gmail.com>
2021-03-11 17:54:45 -0800

    Initial Commit

---
+++ war-challenge/ContentView.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  war-challenge
+//
+//  Created by Kehinde Agboola on 2/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var playerCard = "card6"
+    @State var cpuCard = "card9"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    
+    var body: some View {
+        ZStack {
+            
+            Image("background")
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+           
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack() {Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    
+let playerRand = Int.random(in: 2...14)
+let cpuRand = Int.random(in: 2...14)
+                    
+          // update the cards
+playerCard = "card" + String(playerRand)
+cpuCard = "card" + String(cpuRand)
+        // update the score
+    if playerRand > cpuRand {
+            playerScore += 1
+                    }
+    else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+         
+            
+                }, label: {
+                    Image("dealbutton")
+                })
+               
+                Spacer()
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                    Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                        
+                    }
+                    Spacer()
+                }
+                Spacer()
+               
+                
+            }
+            
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
